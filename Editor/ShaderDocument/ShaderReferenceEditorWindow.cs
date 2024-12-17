49ed5f7ba78b86c8516a3a4bdf909ab6d4d19e3e
chenyu <328800436@qq.com>
2024-12-17 17:18:38 +0800

    更新Painter脚本

--- Editor/ShaderDocument/ShaderReferenceEditorWindow.cs
+++ Editor/ShaderDocument/ShaderReferenceEditorWindow.cs
@@ -131,7 +131,7 @@ namespace yuxuetian
         private ShaderReferenceAbout _about;
         
         //快捷键组合方式 #-shift %-Ctrl &-Alt
-        [MenuItem("ArtTools/ShaderReference #R")]
+        [MenuItem("Window/ShaderReference #R" ,false, 101)]
         public static void ShowWindow()
         {
             EditorWindow window = GetWindow<ShaderReferenceEditorWindow>();
